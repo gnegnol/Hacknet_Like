7818ccf41fc507bbabb42eee46d6b5511c462f13
Sixx93 <sixx93@hotmail.it>
2018-07-12 19:20:33 +0200

    terminale funzionante correttamente

--- Hacknet_Like/Hacknet_Like/Terminal.Designer.cs
+++ Hacknet_Like/Hacknet_Like/Terminal.Designer.cs
@@ -25,7 +25,6 @@
         private void InitializeComponent() {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Terminal));
             this.terminalTextBox = new System.Windows.Forms.TextBox();
-            this.cmdTextbox = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // terminalTextBox
@@ -37,26 +36,11 @@
             this.terminalTextBox.Location = new System.Drawing.Point(0, 0);
             this.terminalTextBox.Multiline = true;
             this.terminalTextBox.Name = "terminalTextBox";
-            this.terminalTextBox.ReadOnly = true;
             this.terminalTextBox.Size = new System.Drawing.Size(824, 374);
             this.terminalTextBox.TabIndex = 1;
             this.terminalTextBox.Text = "Microsoft Windows [Version 10.0.17134.112]\r\n(c) 2018 Microsoft Corporation. All r" +
     "ights reserved.\r\n\r\nC:\\Users\\LadyGay69>";
             this.terminalTextBox.WordWrap = false;
-            // 
-            // cmdTextbox
-            // 
-            this.cmdTextbox.BackColor = System.Drawing.Color.Black;
-            this.cmdTextbox.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.cmdTextbox.Font = new System.Drawing.Font("Ubuntu Mono", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.cmdTextbox.ForeColor = System.Drawing.Color.White;
-            this.cmdTextbox.Location = new System.Drawing.Point(162, 59);
-            this.cmdTextbox.Margin = new System.Windows.Forms.Padding(0);
-            this.cmdTextbox.Multiline = true;
-            this.cmdTextbox.Name = "cmdTextbox";
-            this.cmdTextbox.Size = new System.Drawing.Size(650, 23);
-            this.cmdTextbox.TabIndex = 0;
-            this.cmdTextbox.TextChanged += new System.EventHandler(this.cmdTextbox_TextChanged);
             // 
             // Terminal
             // 
@@ -64,7 +48,6 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(824, 374);
-            this.Controls.Add(this.cmdTextbox);
             this.Controls.Add(this.terminalTextBox);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Terminal";
@@ -78,6 +61,5 @@
         #endregion
 
         private System.Windows.Forms.TextBox terminalTextBox;
-        private System.Windows.Forms.TextBox cmdTextbox;
     }
 }
--- Hacknet_Like/Hacknet_Like/Terminal.cs
+++ Hacknet_Like/Hacknet_Like/Terminal.cs
@@ -14,19 +14,7 @@ namespace Hacknet_Like {
                                      "(c) 2018 Microsoft Corporation. All rights reserved." + Environment.NewLine + Environment.NewLine;
         const string username = "LadyGay69";
         MainBar mainBar;
-
-        private void UpdateCmdTextboxLocation() {
-            Point cmdLocation = new Point();
-
-            Image fakeImage = new Bitmap( 1, 1 );
-            Graphics graphics = Graphics.FromImage( fakeImage );
-            SizeF size = graphics.MeasureString( terminalTextBox.Lines[terminalTextBox.Lines.Length - 1], terminalTextBox.Font );
-
-            cmdLocation.Y = (int)(terminalTextBox.Lines.Length * size.Height) - 14;
-            cmdLocation.X = (int)(terminalTextBox.Lines[terminalTextBox.Lines.Length - 1].Length * 8.3); //8.5
-
-            cmdTextbox.Location = cmdLocation;
-        }
+        private string prompt = "C:\\Users\\" + username + ">";
 
         public Terminal() {
             InitializeComponent();
@@ -39,32 +27,157 @@ namespace Hacknet_Like {
         private void Terminal_Load( object sender, EventArgs e ) {
             terminalTextBox.Text = firstOpenText + "C:\\Users\\" + username + ">";
             terminalTextBox.SelectionStart = terminalTextBox.SelectionLength = 0;
-            cmdTextbox.KeyPress += cmdTextbox_KeyPressed;
-            
-            UpdateCmdTextboxLocation();
+            terminalTextBox.KeyPress += terminalTextBox_KeyPressed;
+            terminalTextBox.KeyDown += terminalTextBox_KeyDown;
         }
         protected override void OnClosed(EventArgs e ) {
             base.OnClosed(e);
             mainBar.terminalsOpened.Remove( this );
         }
 
-        private void cmdTextbox_KeyPressed( object sender, KeyPressEventArgs e ) {
-            if(e.KeyChar == (char)13) {
-                string cmdText = cmdTextbox.Text;
-                if(cmdTextbox.Text.Contains( "\r" )) {
-                    cmdText = cmdText.Substring( 2, cmdText.Length - 2 );
+        private void terminalTextBox_KeyDown( object sender, KeyEventArgs e ) {
+            // If the caret is anywhere else, set it back when a key is pressed.
+            if(!IsCaretAtWritablePosition() && !(e.Control || IsTerminatorKey( e.KeyCode ))) {
+                MoveCaretToEndOfText();
+            }
+
+            // Prevent caret from moving before the prompt
+            if(e.KeyCode == Keys.Left && IsCaretJustBeforePrompt()) {
+                e.Handled = true;
+            } else if(e.KeyCode == Keys.Down) {
+                //if(commandHistory.DoesNextCommandExist()) {
+                //    ReplaceTextAtPrompt( commandHistory.GetNextCommand() );
+                //}
+                e.Handled = true;
+            } else if(e.KeyCode == Keys.Up) {
+                //if(commandHistory.DoesPreviousCommandExist()) {
+                //    ReplaceTextAtPrompt( commandHistory.GetPreviousCommand() );
+                //}
+                e.Handled = true;
+            } else if(e.KeyCode == Keys.Right) {
+                // Performs command completion
+                //string currentTextAtPrompt = GetTextAtPrompt();
+                //string lastCommand = commandHistory.LastCommand;
+
+                //if(lastCommand != null && (currentTextAtPrompt.Length == 0 || lastCommand.StartsWith( currentTextAtPrompt ))) {
+                //    if(lastCommand.Length > currentTextAtPrompt.Length) {
+                //        this.AddText( lastCommand[currentTextAtPrompt.Length].ToString() );
+                //    }
+                //}
+            }
+        }
+        private void terminalTextBox_KeyPressed( object sender, KeyPressEventArgs e ) {
+            // Handle backspace
+            if(e.KeyChar == (char)8 && IsCaretJustBeforePrompt()) {
+                e.Handled = true;
+                return;
+            }
+
+            if(IsTerminatorKey( e.KeyChar )) {
+                e.Handled = true;
+                string currentCommand = GetTextAtPrompt();
+                if(currentCommand.Length != 0) {
+                    printLine();
+                    //((ShellControl)this.Parent).FireCommandEntered( currentCommand );
+                    //commandHistory.Add( currentCommand );
                 }
-                
-                terminalTextBox.Text += cmdText + Environment.NewLine + "C:\\Users\\" + username + ">";
-                
-                cmdTextbox.Text = "";
-                
-                UpdateCmdTextboxLocation();
+                printPrompt();
             }
         }
 
-        private void cmdTextbox_TextChanged( object sender, EventArgs e ) {
-            UpdateCmdTextboxLocation();
+
+        private void printPrompt() {
+            string currentText = terminalTextBox.Text;
+            if(currentText.Length != 0 && currentText[currentText.Length - 1] != '\n')
+                printLine();
+            AddText( prompt );
+        }
+
+        private void printLine() {
+            AddText( System.Environment.NewLine );
+        }
+
+        private string GetCurrentLine() {
+            if(terminalTextBox.Lines.Length > 0)
+                return (string)terminalTextBox.Lines.GetValue( terminalTextBox.Lines.GetLength( 0 ) - 1 );
+            else
+                return "";
+        }
+
+        private string GetTextAtPrompt() {
+            return GetCurrentLine().Substring( prompt.Length );
+        }
+
+        private void ReplaceTextAtPrompt( string text ) {
+            string currentLine = GetCurrentLine();
+            int charactersAfterPrompt = currentLine.Length - prompt.Length;
+
+            if(charactersAfterPrompt == 0)
+                AddText( text );
+            else {
+                terminalTextBox.Select( terminalTextBox.TextLength - charactersAfterPrompt, charactersAfterPrompt );
+                terminalTextBox.SelectedText = text;
+            }
+        }
+
+        private bool IsCaretAtCurrentLine() {
+            return terminalTextBox.TextLength - terminalTextBox.SelectionStart <= GetCurrentLine().Length;
+        }
+
+        private void MoveCaretToEndOfText() {
+            terminalTextBox.SelectionStart = terminalTextBox.TextLength;
+            terminalTextBox.ScrollToCaret();
+        }
+
+        private bool IsCaretJustBeforePrompt() {
+            return IsCaretAtCurrentLine() && GetCurrentCaretColumnPosition() == prompt.Length;
+        }
+
+        private int GetCurrentCaretColumnPosition() {
+            string currentLine = GetCurrentLine();
+            int currentCaretPosition = terminalTextBox.SelectionStart;
+            return (currentCaretPosition - terminalTextBox.TextLength + currentLine.Length);
+        }
+
+        private bool IsCaretAtWritablePosition() {
+            return IsCaretAtCurrentLine() && GetCurrentCaretColumnPosition() >= prompt.Length;
+        }
+
+        private void SetPromptText( string val ) {
+            string currentLine = GetCurrentLine();
+            terminalTextBox.Select( 0, prompt.Length );
+            terminalTextBox.SelectedText = val;
+
+            prompt = val;
+        }
+
+        public string Prompt
+        {
+            get { return prompt; }
+            set { SetPromptText( value ); }
+        }
+
+        //public string[] GetCommandHistory() {
+        //    return commandHistory.GetCommandHistory();
+        //}
+
+        public void WriteText( string text ) {
+            AddText( text );
+        }
+
+
+        private bool IsTerminatorKey( Keys key ) {
+            return key == Keys.Enter;
+        }
+
+        private bool IsTerminatorKey( char keyChar ) {
+            return ((int)keyChar) == 13;
+        }
+
+        // Substitute for buggy AppendText()
+        private void AddText( string text ) {
+            terminalTextBox.Text += text;
+            MoveCaretToEndOfText();
         }
     }
 }
